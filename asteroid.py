25b2463767b85a9bb8667af7510a75e7461c1537
Nessie <lachlan.schofield@outlook.com.au>
2026-08-20 14:47:47 -0400

    Asteroids_Base_V1

--- asteroid.py
+++ asteroid.py
@@ -1,8 +1,11 @@
 
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -13,4 +16,25 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
-    
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        angle =random.uniform(20, 50)
+        self.velocity.rotate(angle)
+        self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = self.velocity.rotate(angle) * 1.2
+
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = self.velocity.rotate(-angle) * 1.2
+
+
+
+        
--- main.py
+++ main.py
@@ -53,12 +53,12 @@ def main():
                 log_event("player_hit")
                 print(f"Game over!")
                 sys.exit()
-
+                
         for shot in shots:
             for asteroid in asteroids:
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for obj in drawable:
