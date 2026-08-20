4068ed50befac0dd4db63c3ee53323b1635726c0
Nessie <lachlan.schofield@outlook.com.au>
2026-08-20 11:15:22 -0400

    Asteroids_Step_3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,8 +10,9 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         dt = clock.tick(60) / 1000
         log_state()
@@ -18,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(("black"))
+        player.draw(screen)
         pygame.display.flip()
 
 
