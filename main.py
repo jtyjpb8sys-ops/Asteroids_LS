f18958706f90dcd3a4ecba287bc539dd21a9b338
Nessie <lachlan.schofield@outlook.com.au>
2026-08-20 09:58:39 -0400

    Asteroids_Step_2

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        dt = clock.tick(60) / 1000
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
